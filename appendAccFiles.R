d2f02542a757b7b9d6eb38f122de7065d148a567
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-08-03 10:21:22 -0400

    Generate time series using milliseconds distributed evenly within a second

--- appendAccFiles.R
+++ appendAccFiles.R
@@ -14,7 +14,7 @@ startTime = as.POSIXct("2017-11-15 18:00:00.001")
 #### Append date time stamp column
 appendedFile$HEADER_TIME_STAMP <- startTime
 
-#test <- head(appendedFile, 800)
+test <- head(appendedFile, 800)
 
 
 
@@ -28,36 +28,38 @@ for (i in 1:nrow(appendedFile)){
   
   print(paste0("i is: ", i))
   
-  if (samples < samplingRate){
-    #### only increment milliseconds
-    ### Then incremement the samples
-    
-    startTime = startTime + 0.001
-    
-    appendedFile$HEADER_TIME_STAMP[i] = startTime
-    samples = samples + 1
-    print(paste0("samples: ", samples))
-    
-  } else if (samples == samplingRate){
-    #### Increment only seconds
-    #### set samples to 0
-    print(paste0("samples reached: ", samples))
-    startTime = round(startTime, units = "secs")
-    startTime = startTime + 1.001
-    
-    
-    #### Set milliseconds back to 0
-    
-    
-    appendedFile$HEADER_TIME_STAMP[i] = startTime
-    samples = 0
-    secondCount = secondCount + 1
-    
-  }else if (samples > samplingRate){
-    ### Do nothing
-    
-    
-  }
+  # if (samples < samplingRate){
+  #   #### only increment milliseconds
+  #   ### Then incremement the samples
+  #   
+  #   startTime = startTime + 0.0125
+  #   
+  #   test$HEADER_TIME_STAMP[i] = startTime
+  #   samples = samples + 1
+  #   ##print(paste0("samples: ", samples))
+  #   
+  # } else if (samples == samplingRate){
+  #   #### Increment only seconds
+  #   #### set samples to 0
+  #   ##print(paste0("samples reached: ", samples))
+  #   startTime = round(startTime, units = "secs")
+  #   startTime = startTime + 1
+  #   
+  #   
+  #   #### Set milliseconds back to 0
+  #   
+  #   
+  #   test$HEADER_TIME_STAMP[i] = startTime
+  #   samples = 0
+  #   secondCount = secondCount + 1
+  #   
+  # }else if (samples > samplingRate){
+  #   ### Do nothing
+  #   
+  #   
+  # }
+  appendedFile$HEADER_TIME_STAMP[i] = startTime
+  startTime = startTime + 0.0125
 
   
 }
